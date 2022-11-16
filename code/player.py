686c0fb058b4f97f0daca42f90868f91152fe23d
RB-Nostalgia <81151894+RB-Nostalgia@users.noreply.github.com>
2022-11-16 13:18:35 -0600

    added the ability to use player images
    You can edit the players image by going into the player.py file inside the code folder and changing "self.status" inside of the init method

--- code/player.py
+++ code/player.py
@@ -8,9 +8,9 @@ class Player(pygame.sprite.Sprite):
         super().__init__(group)
 
         self.import_assets()
-        self.status = 'down_idle'
+        self.status = 'left_water'
         self.frame_index = 0
-
+ 
         #general setup
         self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center = pos)
@@ -22,16 +22,14 @@ class Player(pygame.sprite.Sprite):
     
     def import_assets(self):    
         self.animations = {'up': [], 'down' : [], 'left' : [], 'right' : [],
-        'right_idle' : [], 'left_idle' : [], 'up_idle' : [], 'down_idle' : [],
-        'right_hoe' : [], 'left_hoe' : [], 'up_hoe' : [], 'down_hoe' : [],
-        'right_axe' : [], 'left_axe' : [], 'up_axe' : [], 'down_axe' : [],
-        'right_water' : [], 'left_water' : [], 'up_water' : [], 'down_water' : []}
+                            'right_idle' : [], 'left_idle' : [], 'up_idle' : [], 'down_idle' : [],
+                            'right_hoe' : [], 'left_hoe' : [], 'up_hoe' : [], 'down_hoe' : [],
+                            'right_axe' : [], 'left_axe' : [], 'up_axe' : [], 'down_axe' : [],
+                            'right_water' : [], 'left_water' : [], 'up_water' : [], 'down_water' : []}
 
         for animation in self.animations.keys():
             full_path = '../graphics/character/' + animation
-            self.animations[animation] = import_folder(full_path)    
-            fileExists = os.path.exists(full_path)
-            print(fileExists)
+            self.animations[animation] = import_folder(full_path)
 
     def input(self):
         keys = pygame.key.get_pressed()
